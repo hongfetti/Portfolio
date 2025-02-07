815eff88434785c81b4903a06848a4b63659df08
hongfetti <ryan.hong1994@gmail.com>
2025-02-07 16:21:57 -0500

    figured out the cards and have them working. app is functional and meets min acceptance criteria, ready for submission however lots of work still to be done

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,9 +1,26 @@
 export default function List(props) {
     return (
-        <ul className='list-group'>
+        <>
         {props.projects.map(item => (
-            <li className='list-group-item' key={item.id}>{item.projectName}</li>
+            <div className='col-md-4 mb-4' key={item.id}>
+                <div className='card shadow-sm'>
+                    <img src={item.projectImg} className='card-img-top' alt={item.projectName} />
+                    <div className='card-body'>
+                        <h5 className='card-title'>{item.projectName}</h5>
+                        <div className='d-flex justify-content-between'>
+                            <a href={item.projectRepoLink} className='btn btn-primary' target='_blank' rel='noopener noreferrer'>
+                            Github Repo
+                            </a>
+                            {item.projectDeployedLink && (
+                            <a href={item.projectDeployedLink} className='btn btn success' target='_blank' rel='noopener noreferrer'>
+                                Live Demo
+                            </a>
+                            )}
+                        </div>
+                    </div>
+                </div>
+            </div>
         ))}
-        </ul>
+        </>
     );
 }
--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -7,54 +7,56 @@ import weather from '../assets/images/weather.jpg'
 import portfolio from '../assets/images/portfolio.jpg'
 
 const projects = [
-    {
-      id: 1,
-      projectName: "Saye's Shelter (Project 1)",
-      projectImg: {shelter},
-      projectRepoLink: 'https://github.com/je210506/Project-1-Group-6',
-      projectDeployedLink: '',
-    },
-    {
-      id: 2,
-      projectName: 'README Generator',
-      projectImg: {README},
-      projectRepoLink: 'https://github.com/hongfetti/README-generator',
-      projectDeployedLink: '',
-    },
-    {
-      id: 3,
-      projectName: 'Vehicle Builder',
-      projectImg: {vehicleBuilder},
-      projectRepoLink: 'https://github.com/hongfetti/Vehicle-Builder',
-      projectDeployedLink: '',
-    },
-    {
-      id: 4,
-      projectName: 'Weather Dashboard',
-      projectImg: {weather},
-      projectRepoLink: 'https://github.com/hongfetti/weather-dashboard',
-      projectDeployedLink: 'https://weather-dashboard-fvb6.onrender.com',
-    },
-    {
-      id: 5,
-      projectName: 'Employee Tracker',
-      projectImg: {empTracker},
-      projectRepoLink: 'https://github.com/hongfetti/employee-tracker',
-      projectDeployedLink: '',
-    },
-    {
-      id: 6,
-      projectName: 'Portfolio',
-      projectImg: {portfolio},
-      projectRepoLink: 'https://github.com/hongfetti/Portfolio',
-      projectDeployedLink: '',
-    },
-  ];
-  
+  {
+    id: 1,
+    projectName: "Saye's Shelter (Project 1)",
+    projectImg: shelter,
+    projectRepoLink: 'https://github.com/je210506/Project-1-Group-6',
+    projectDeployedLink: '',
+  },
+  {
+    id: 2,
+    projectName: 'README Generator',
+    projectImg: README,
+    projectRepoLink: 'https://github.com/hongfetti/README-generator',
+    projectDeployedLink: '',
+  },
+  {
+    id: 3,
+    projectName: 'Vehicle Builder',
+    projectImg: vehicleBuilder,
+    projectRepoLink: 'https://github.com/hongfetti/Vehicle-Builder',
+    projectDeployedLink: '',
+  },
+  {
+    id: 4,
+    projectName: 'Weather Dashboard',
+    projectImg: weather,
+    projectRepoLink: 'https://github.com/hongfetti/weather-dashboard',
+    projectDeployedLink: 'https://weather-dashboard-fvb6.onrender.com',
+  },
+  {
+    id: 5,
+    projectName: 'Employee Tracker',
+    projectImg: empTracker,
+    projectRepoLink: 'https://github.com/hongfetti/employee-tracker',
+    projectDeployedLink: '',
+  },
+  {
+    id: 6,
+    projectName: 'Portfolio',
+    projectImg: portfolio,
+    projectRepoLink: 'https://github.com/hongfetti/Portfolio',
+    projectDeployedLink: '',
+  },
+];
+
 export default function Project() {
-    return (
-        <div className="card">
-            <List projects={projects}/>
-        </div>
-    );
+  return (
+    <div className="container">
+      <div className="row">
+        <List projects={projects} />
+      </div>
+    </div>
+  );
 }
