7ef6f93f3958c7471ab9d7500b5ae1b46a6778a5
hongfetti <ryan.hong1994@gmail.com>
2025-02-06 19:16:57 -0500

    all page routes working - still need to make portfolio cards functional but will push up changes for now as stable

--- src/App.jsx
+++ src/App.jsx
@@ -4,59 +4,15 @@
 import './App.css'
 import { Outlet } from 'react-router-dom'
 
-import List from './components/List.jsx'
 import Footer from './components/Footer.jsx'
 import Header from './components/Header.jsx'
-// use this for the project cards - map over projectCardArray in function App() - see activity 10
+// use this for the project cards - map over projects in function App() - see activity 10
 
-// const projectCardArray = [
-//   {
-    // id: 1,
-//     projectName: 'Saye's Shelter (Project 1)',
-//     projectImg: {},
-//     projectRepoLink: '',
-//     projectDeployedLink?: '',
-//   },
-//   {
-    // id: 2,
-//     projectName: 'README Generator',
-//     projectImg: {},
-//     projectRepoLink: '',
-//     projectDeployedLink?: '',
-//   },
-//   {
-    // id: 3,
-//     projectName: 'Vehicle Builder',
-//     projectImg: {},
-//     projectRepoLink: '',
-//     projectDeployedLink?: '',
-//   },
-//   {
-    // id: 4,
-//     projectName: 'Weather Dashboard',
-//     projectImg: {},
-//     projectRepoLink: '',
-//     projectDeployedLink?: '',
-//   },
-//   {
-    // id: 5,
-//     projectName: 'Employee Tracker',
-//     projectImg: {},
-//     projectRepoLink: '',
-//     projectDeployedLink?: '',
-//   },
-//   {
-    // id: 6,
-//     projectName: 'Portfolio',
-//     projectImg: {},
-//     projectRepoLink: '',
-//     projectDeployedLink?: '',
-//   },
-// ];
+
 
 function App() {
 
-  // const [page, setPage] = useState(?)
+  // const [state, setState] = useState('/')
 
   return (
     <>
--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,13 +1,53 @@
+import List from './List.jsx'
 
+const projects = [
+    {
+      id: 1,
+      projectName: "Saye's Shelter (Project 1)",
+      projectImg: {},
+      projectRepoLink: '',
+      projectDeployedLink: '',
+    },
+    {
+      id: 2,
+      projectName: 'README Generator',
+      projectImg: {},
+      projectRepoLink: '',
+      projectDeployedLink: '',
+    },
+    {
+      id: 3,
+      projectName: 'Vehicle Builder',
+      projectImg: {},
+      projectRepoLink: '',
+      projectDeployedLink: '',
+    },
+    {
+      id: 4,
+      projectName: 'Weather Dashboard',
+      projectImg: {},
+      projectRepoLink: '',
+      projectDeployedLink: '',
+    },
+    {
+      id: 5,
+      projectName: 'Employee Tracker',
+      projectImg: {},
+      projectRepoLink: '',
+      projectDeployedLink: '',
+    },
+    {
+      id: 6,
+      projectName: 'Portfolio',
+      projectImg: {},
+      projectRepoLink: '',
+      projectDeployedLink: '',
+    },
+  ];
 export default function Project() {
     return (
         <div className="card">
-            <button onClick={() => setCount((count) => count + 1)}>
-                count is {count}
-            </button>
-            <p>
-                This card is a placeholder for a Portfolio Project card
-            </p>
+            <List projects={projects}/>
         </div>
     );
 }
